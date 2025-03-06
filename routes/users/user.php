dae263602a60b110e6916df5ad8b31a4edfd5db2
Abraham Rodriguez <arm.developer00@gmail.com>
2025-03-06 19:14:38 +0100

    arreglos

--- routes/users/user.php
+++ routes/users/user.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;    
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function(){
@@ -13,6 +14,7 @@ Route::middleware(['guest'])->group(function(){
 
 
 Route::middleware(['auth'])->group(function(){
+    Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/profile', [UserController::class, 'showProfile'])->name('user.showProfile');
     Route::get('/logout', [UserController::class, 'doLogout'])->name('user.doLogout');
 });
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
+use App\Http\Controllers\UserController;
 
 Route::prefix('users')->group(base_path('routes/users/user.php'));
 Route::prefix('posts')->group(base_path('routes/posts/post.php'));
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+});
+
+
+Route::middleware(['guest'])->group(function(){
+    Route::get('/login', [UserController::class, 'showLogin'])->name('login');
+    Route::get('/register', [UserController::class, 'showRegister'])->name('register');
+});
 
